b4f8261a859539602cd482524348eb63733215d9
mattg <mattg@DESKTOP-L385G9G>
2019-09-29 13:42:54 -0500

    Updated code.

--- src/main/java/PaintJob.java
+++ src/main/java/PaintJob.java
@@ -1,24 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
-/**
- *
- * @author 0749489
- */import java.util.Scanner;
 public class PaintJob {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        
-        System.out.println("Please enter the square footage of the room.");
-        double sqFtOfWallSpace = scan.nextDouble();
+        double sqFtOfWallSpace = 0;
+        int numberOfRooms = 0;
+
+        while (numberOfRooms <= 0) {
+           String userInput_Rooms = JOptionPane.showInputDialog(null, "Please enter how many rooms will be painted.");
+           numberOfRooms = Integer.parseInt(userInput_Rooms);
+            //numberOfRooms = scan.nextInt();
+
+            if (numberOfRooms < 0) {
+                JOptionPane.showMessageDialog(null, "Please enter a positive number.");
+            }
+        }
+
+        for (int i = 1; i <= numberOfRooms; i++) {
+            String userInput_Area = JOptionPane.showInputDialog(null, "Please enter the square footage of room " + i + ".");
+            double sqFtCounter = Double.parseDouble(userInput_Area);
+            sqFtOfWallSpace += sqFtCounter;
+        }
+
         PaintJobEstimator myPaintJob = new PaintJobEstimator();
         myPaintJob.setSqFtOfWallSpace(sqFtOfWallSpace);
         myPaintJob.setGallonsOfPaint();
+        myPaintJob.setHoursOfLabor();
         myPaintJob.setCostOfPaint();
-        System.out.println(myPaintJob.getCostOfPaint());
-     System.out.println(myPaintJob.getGallonsOfPaint());
+        myPaintJob.setCostOfLabor();
+        myPaintJob.setTotalCost();
+
+        DecimalFormat df = new DecimalFormat("#.00");
+        df.setRoundingMode(RoundingMode.CEILING);
+
+        JOptionPane.showMessageDialog(null,
+                "Total gallons of paint: " + df.format(myPaintJob.getGallonsOfPaint()) + "\n"
+                + "Total hours of labor: " + df.format(myPaintJob.getHoursOfLabor()) + "\n"
+                + "Total cost of paint: $" + df.format(myPaintJob.getCostOfPaint()) + "\n"
+                + "Total cost of labor: $" + df.format(myPaintJob.getCostOfLabor()) + "\n"
+                + "Total cost: $" + df.format(myPaintJob.getTotalCost()));
     }
 }
